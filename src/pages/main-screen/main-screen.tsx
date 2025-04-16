8e9bca6584894fde17a0d39fd48069489a7c9a1b
Vladislav KS <spike.kvs@gmail.com>
2025-04-16 12:58:31 +0300

    add React link

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,5 +1,6 @@
 import PlaceCard from '../../components/place-card';
 import MainLogo from '../../components/main-logo/main-logo.tsx';
+import {Link} from 'react-router-dom';
 
 type MainScreeProps = {
   placeCardCount: number;
@@ -11,29 +12,30 @@ export default function MainScreen({placeCardCount} : MainScreeProps): JSX.Eleme
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__lef">
+            <div className="header__left">
               <MainLogo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link className="header__nav-link header__nav-link--profile" to="/favorites">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
+                  <Link className="header__nav-link" to="/login">
                     <span className="header__signout">Sign out</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
           </div>
         </div>
       </header>
+
 
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
