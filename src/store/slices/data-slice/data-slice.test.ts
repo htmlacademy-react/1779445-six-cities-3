e79e5d94681140f059ae649b407b1cb7177e2b8f
Vladislav KS <spike.kvs@gmail.com>
2025-06-16 20:43:18 +0300

    fix lint data-slice

--- src/store/slices/data-slice/data-slice.test.ts
+++ src/store/slices/data-slice/data-slice.test.ts
@@ -22,6 +22,7 @@ const createInitialState = (): DataState => ({
   isFavorite: false,
   favoriteOffers: [],
   isOfferLoading: false,
+  isFavoriteLoading: false,
 });
 
 describe('Data slice reducer', () => {
